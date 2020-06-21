20b641aa631fce0669817355a4c19983a7c49d1a
Sean M <srmcquay@gmail.com>
2020-06-21 12:03:45 -0500

    Added support for button sstyle type.

--- src/direflow-components/ButtonComponent/ButtonComponent.js
+++ src/direflow-components/ButtonComponent/ButtonComponent.js
@@ -1,24 +1,36 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import {
+    DefaultButton,
     PrimaryButton
 } from '@fluentui/react';
 
 const ButtonComponent = (props) => {
     const {
+        isPrimaryButton,
         label
     } = props;
 
+    const defaultButtonProps = {
+        text: label
+    };
+
     return (
-        <PrimaryButton text={label} />
+        isPrimaryButton ? (
+            <PrimaryButton {...defaultButtonProps} />
+        ) : (
+            <DefaultButton {...defaultButtonProps} />
+        )
     );
 };
 
 ButtonComponent.propTypes = {
+    isPrimaryButton: PropTypes.bool,
     label: PropTypes.string
 };
 
 ButtonComponent.defaultProps = {
+    isPrimaryButton: false,
     label: ''
 };
 
